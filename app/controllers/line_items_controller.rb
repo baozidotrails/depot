cceb6185e9628183e5013cbee44811c221325bf4
Baozi <baozi.rails@gmail.com>
2014-08-27 10:55:48 +0800

    Handling errors

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,13 +28,13 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
         session[:counter] = nil
-
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        flash[:success] = "Item add in cart successfully."
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -75,6 +75,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     time = Time.now
     time.to_formatted_s format
   end
+
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success',
+      info:    'alert-info',
+      error:   'alert-danger',
+      warning: 'alert-warning' }[flash_type.to_sym] || flash_type.to_s
+  end
 end
